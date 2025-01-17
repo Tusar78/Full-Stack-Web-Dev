6f5b83ea02373bbc3202c6f7ec2bfa6bafa5de54
Tusar <tusarahmed141@gmail.com>
2025-01-17 19:41:37 +0600

    Falsy value remove on the array using imparetive loop

--- lecture_06/app.js
+++ lecture_06/app.js
@@ -1,14 +1,57 @@
-const numbers = [2, 5, 6, 7, 89, 100];
+// const numbers = [2, 5, 6, 7, 89, 100];
 
-let sumSeries = "";
-let sum = 0;
-for (let i = 0; i < numbers.length; i++) {
-    if (i != numbers.length - 1) {
-        sumSeries += numbers[i] + ' + ';        
-    } else {
-        sumSeries += numbers[i];        
+// let sumSeries = "";
+// let sum = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//     if (i != numbers.length - 1) {
+//         sumSeries += numbers[i] + ' + ';
+//     } else {
+//         sumSeries += numbers[i];
+//     }
+//   sum += numbers[i];
+// }
+
+// console.log(`${sumSeries} = ${sum}`);
+
+// const nums = [1, 2, 3, 4, 5, 6];
+
+// const double = [];
+
+// for (let i = 0; i < nums.length; i++) {
+//     double.push(nums[i] * 2);
+// }
+
+// console.log(double);
+
+// const dbl = nums.map(num => num * 2);
+// console.log(dbl);
+
+// const numbers = [2, 5, 6, 7, 89, 100];
+// numbers.forEach(function (v, i, arr) {
+//     console.log(v, i, arr);
+// })
+
+// numbers.map(function() {
+//     console.log(arguments);
+// })
+
+const arr = [1, 2, 3, null, false, 4, 5, "", "test", 6, 7];
+
+let count = 0;
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr.length - 1; j++) {
+    if (!arr[j] || typeof arr[j] !== "number") {
+      arr[j] = arr[j + 1];
+      arr[j + 1] = undefined;
     }
-  sum += numbers[i];
+  }
+
+  if (arr[i] == undefined) {
+    count++;
+  }
 }
 
-console.log(`${sumSeries} = ${sum}`);
+arr.length -= count;
+
+console.log(arr);
+
