983d320099004b8ed1e4d99c08aff14a79a339f7
Tusar <tusarahmed141@gmail.com>
2025-01-17 19:51:12 +0600

    Complex array update using imparetive way

--- lecture_06/app.js
+++ lecture_06/app.js
@@ -35,23 +35,40 @@
 //     console.log(arguments);
 // })
 
-const arr = [1, 2, 3, null, false, 4, 5, "", "test", 6, 7];
+// const arr = [1, 2, 3, null, false, 4, 5, "", "test", 6, 7];
 
-let count = 0;
+// let count = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   for (let j = 0; j < arr.length - 1; j++) {
+//     if (!arr[j] || typeof arr[j] !== "number") {
+//       arr[j] = arr[j + 1];
+//       arr[j + 1] = undefined;
+//     }
+//   }
+
+//   if (arr[i] == undefined) {
+//     count++;
+//   }
+// }
+
+// arr.length -= count;
+
+// console.log(arr);
+
+const arr = [
+	{ id: 1, value: 10 },
+	{ id: 2, value: 20 },
+	{ id: 3, value: 30 },
+	{ id: 4, value: 40 },
+	{ id: 5, value: 50 }
+]
+
 for (let i = 0; i < arr.length; i++) {
-  for (let j = 0; j < arr.length - 1; j++) {
-    if (!arr[j] || typeof arr[j] !== "number") {
-      arr[j] = arr[j + 1];
-      arr[j + 1] = undefined;
-    }
-  }
-
-  if (arr[i] == undefined) {
-    count++;
-  }
+    if (arr[i].id == 3) {
+        arr[i].value = 300
+    }    
 }
 
-arr.length -= count;
-
 console.log(arr);
+
 
