c55bc116f9b6ba7e32b836339725305a7481972c
Tusar <tusarahmed141@gmail.com>
2025-01-25 12:22:23 +0600

    implement amezing iterator

---
+++ lecture_11/app.js
@@ -0,0 +1,60 @@
+// const arr = [1,2,3,4]
+
+// let index = 0;
+// const next =()=> {
+//     return arr[index++]
+// }
+
+// console.log(next());
+// console.log(next());
+// console.log(next());
+// console.log(next());
+
+
+// const myName = [1,2,3,4,5]
+// const myNameIterator = myName[Symbol.iterator]()
+// console.log(myNameIterator.next());
+// console.log(myNameIterator.next());
+// console.log(myNameIterator.next());
+// console.log(myNameIterator.next());
+// console.log(myNameIterator.next());
+// console.log(myNameIterator.next());
+
+
+const range = {
+    start: 0,
+    stop: 50,
+    step: 5
+}
+
+range[Symbol.iterator] = function() {
+    let current = this.start
+    let stop = this.stop;
+    let step = this.step;
+
+    return {      
+        next(){
+            const obj = {
+                value: current > stop ? undefined : current,
+                done: current > stop
+            }
+            current += step;
+            return obj;
+        }
+    }
+}
+
+const myIterator = range[Symbol.iterator]();
+console.log(myIterator.next());
+console.log(myIterator.next());
+console.log(myIterator.next());
+console.log(myIterator.next());
+console.log(myIterator.next());
+
+for (const v of range) {
+    console.log(v);
+    
+}
+ 
+
+
